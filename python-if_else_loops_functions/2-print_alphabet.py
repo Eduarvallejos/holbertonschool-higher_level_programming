88f07dd7bb8203753694f71c199326a8b1d79c88
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-09-23 20:30:00 -0700

    exercises[0:13] of if/else , loops, functions

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 alphabet = ''.join([chr(letra) for letra in range(ord('a'), ord('z') + 1)])
-formatted_alphabet = f"{alphabet}"
+formatted_alphabet = "{}".format(alphabet)
 print(formatted_alphabet)
