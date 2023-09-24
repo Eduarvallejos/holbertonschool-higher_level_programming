c90fa5afee9c914fa97e093453f11cea007b6882
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-09-24 09:28:25 -0700

    exercises[0:13] of if/else , loops, functions

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    resultado = ""
+    for caracter in str:
+        if 'a' <= caracter <= 'z':
+            resultado += chr(ord(caracter) - ord('a') + ord('A'))
+        else:
+            resultado += caracter
+    print(resultado)
