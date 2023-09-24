e7a2d01d0c292cb2061e591ece393829f83de3a8
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-09-23 20:49:33 -0700

    exercises[0:13] of if/else , loops, functions

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+output = ''
 for letra in range(ord('a'), ord('z') + 1):
     if chr(letra) not in ['e', 'q']:
-        print(chr(letra), end='')
+        output += chr(letra)
+
+print("{}".format(output), end= '')
