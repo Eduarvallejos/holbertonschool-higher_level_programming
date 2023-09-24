9bdfd7cd29ab8cecd8aad47ad6553c9d761face9
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-09-24 11:26:50 -0700

    exercises[0:13] of if/else , loops, functions

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-     for numero in range(1, 101):
+    for numero in range(1, 101):
         if numero % 3 == 0 and numero % 5 == 0:
             print("FizzBuzz", end=' ')
         elif numero % 3 == 0:
