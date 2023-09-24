9658f1d2fa2fe8097894eb2ab5fbdaceda19bbb4
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-09-23 21:34:39 -0700

    exercises[0:13] of if/else , loops, functions

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 output = ""
 for number in range(100):
-        output += "{}{}".format(number,', ' if number < 99 else '\n')
+    output += "{:02d}{}".format(number,', ' if number < 99 else '\n')
 print(output, end='')
