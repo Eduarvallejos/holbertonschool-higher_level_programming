c859f9f55948b3ce34074ebda67db1909045fc64
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-09-24 18:12:44 -0700

    Data Structures exercises:list,tuples[0:13]

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    last_num = None
     for num in reversed(my_list):
         print("{}".format(num))
+        last_num = num
+        return last_num
