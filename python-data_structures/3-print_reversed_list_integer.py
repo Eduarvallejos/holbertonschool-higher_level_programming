2f5bbab1c6c43baa8f7fa53b45cd2cfef65e71f1
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-09-24 17:19:38 -0700

    Data Structures exercises:list,tuples[0:13]

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,4 @@
 
 def print_reversed_list_integer(my_list=[]):
     for num in reversed(my_list):
-        if isinstanse(num, int):
-            print("{}".format(num))
+        print("{}".format(num))
