ab428f932bed95a3be587b407c78dc2543fb30d5
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-09-25 20:58:31 -0700

    exercises from More Data Structures: Set, Dictionary[0-12]

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda x: x ** 2, matrix))
+    def square(x):
+        return x ** 2 if isinstance(x, int) else x
+
+    result_matrix = list(map(lambda row: list(map(square, row)), matrix))
+    return result_matrix
