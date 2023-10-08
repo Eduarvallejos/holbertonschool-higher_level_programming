4b4aa504397835dbcb7ad7e51354b2325e6c4f65
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-10-07 17:02:11 -0700

    exercises from More Data Structures: Set, Dictionary[0-12]

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
-    # Creamos una nueva lista para almacenar el resultado
     new_list = []
-    # Iteramos a través de los elementos de la lista original
     for item in my_list:
-        # Si el elemento actual es igual al elemento a buscar, lo reemplazamos
         if item == search:
             new_list.append(replace)
         else:
