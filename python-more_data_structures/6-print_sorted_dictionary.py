1c2b9604b663ee13e0d762eb5b3cfb55327b16c4
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-10-07 19:03:20 -0700

    exercises from More Data Structures: Set, Dictionary[0-12]

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    ordered_dictionary = sorted(a_dicionary.keys())
+    ordered_dictionary = sorted(a_dictionary.keys())
     for key in ordered_dictionary:
         print(f"{key}: {a_dictionary[key]}")
