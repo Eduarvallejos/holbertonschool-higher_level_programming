9e8e4f021328cfb2cd1daacadb46f4f138e70de4
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-10-07 20:44:44 -0700

    exercises from More Data Structures: Set, Dictionary[0-12]

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -3,4 +3,4 @@ def simple_delete(a_dictionary, key=""):
     # Verificar si la clave existe en el diccionario
     if key in a_dictionary:
         del a_dictionary[key]
-        return a_dictionary  # Devolver el diccionario actualizado
+    return a_dictionary  # Devolver el diccionario actualizado
