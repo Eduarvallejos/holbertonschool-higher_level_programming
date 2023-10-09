8db10c4c6ec1cd996bd783199252961ec3bfe9c8
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-10-08 21:38:39 -0700

    class and object exercises [0-6]

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,6 +5,9 @@ Este modulo representa una clase square
 
 
 class Square:
+    """
+    Este es una clase que define un cuadrado
+    """
     def __init__(self, size):
         """
         Inicializa una instancia de Square con un tamaño dado.
