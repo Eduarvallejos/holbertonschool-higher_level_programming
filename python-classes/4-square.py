4fd21a2a2db1ead03a8c3b572da97952e8364b4b
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-10-08 19:59:11 -0700

    class and object exercises [0-6]

---
+++ python-classes/4-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        """
+        Inicializa una instancia de Square con un tamaño opcional dado.
+
+        Args:
+            size (int, opcional): El tamaño del cuadrado
+            (verificación de tipo y valor).
+                El valor predeterminado es 0.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Getter para obtener el valor del tamaño del cuadrado.
+
+        Returns:
+            int: El tamaño del cuadrado.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter para establecer el valor del tamaño del
+        cuadrado con verificación.
+
+        Args:
+            value (int): El nuevo tamaño del cuadrado.
+
+        Raises:
+            TypeError: Si el valor no es un entero.
+            ValueError: Si el valor es menor que 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calcula y devuelve el área del cuadrado.
+
+        Returns:
+            int: El área del cuadrado (lado * lado).
+        """
+        return self.__size * self.__size
