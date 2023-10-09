4ee8c64bc6e468b7ae2f3a83eb0b93db8189cb22
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-10-08 19:05:32 -0700

    class and object exercises [0-6]

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        """
+        Inicializa una instancia de Square con un tamaño dado.
+
+        Args:
+            size: El tamaño del cuadrado (sin verificación de tipo o valor).
+        """
+        self.__size = size
