9a5d012f5afb81ba44eba1cff821cbcaed84e210
Eduarvallejos <manuelvallejos2018@gmail.com>
2023-10-08 19:38:39 -0700

    class and object exercises [0-6]

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        """
+        Inicializa una instancia de Square con un tamaño opcional dado.
+
+        Args:
+            size (int, opcional): El tamaño del cuadrado
+            (verificación de tipo y valor).
+                El valor predeterminado es 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
