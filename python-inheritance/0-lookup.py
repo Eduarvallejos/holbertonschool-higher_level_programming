3710a726e789c3b611dea18dff8f4edd87c4b1dd
root <manuelvallejos2018@gmail.com>
2023-10-22 11:56:35 -0700

    exercises on inheritance [0-11]

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,5 +4,5 @@ def lookup(obj):
     Utiliza la funcion dir() para obtener una lista de atributos
     y metodos del objeto
     """
-    attributes_and_methods = dir(obj)
-    return attributes_and_methods
+    list = dir(obj)
+    return list
