6102cd4fe4a39fcce6098d959005488b392ffb5a
root <manuelvallejos2018@gmail.com>
2023-10-22 20:06:59 -0700

    exercises on inheritance [0-11]

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,34 +1,8 @@
 #!/usr/bin/python3
 """
-definimos una funcion BaseGeometry
+definimos una clase Rectangle que herda de BaseGeometry
 """
-
-
-class BaseGeometry:
-    """
-    BaseGemotry nos servira como una clase base para operaciones de
-    geometria
-    """
-    def area(self):
-        """
-        Este metodo lanza una excepcion indicando que area() no
-        esta implimentado.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Verifica si 'value' es una entero y si e mayor o igual que 0,
-        Lanzando excepciones si no cumplen con los requisitos.
-
-        Args:
-        name (str): El nombre del valor que se esta validando.
-        value (int): El valor a ser validado.
-        """
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -41,7 +15,8 @@ class Rectangle(BaseGeometry):
         """
         self.__width = width
         self.__height = height
-        """Llama al método integer_validator de la clase base para validar
+        """
+        Llama al método integer_validator de la clase base para validar
         si el ancho y el alto son enteros.
         """
         self.integer_validator("width", width)
