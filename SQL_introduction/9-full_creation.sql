74142170a5222353a77fb4c768837434335d0a47
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-12-09 19:03:25 -0500

    Se creara la tabla second_table si no exist

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- Este script creara una tabla llamada second_table.
 
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT AUTO_INCREMENT,
+	id INT,
 	name VARCHAR(256),
 	score INT
 );
-INSERT INTO second_table (name, score) VALUES ('Jhon', 10), ('Alex', 3), ('Bob', 14), ('George', 8);
+INSERT INTO second_table (id, name, score) VALUES (1, 'Jhon', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
