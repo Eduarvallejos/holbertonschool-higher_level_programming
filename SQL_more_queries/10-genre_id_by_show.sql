0ddd79492f67b9153998872efefd40faaffa82dc
root <manuelvallejos2018@gmail.com>
2023-12-10 11:55:33 -0800

    Enumera los programas contenidos en 'hbtn_0d_tvshows'

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- Enumera todos los programas contenidos en hbtn_0d_tvshows
+--Que tengan al menos un genero vinculado.
+
+SELECT 
+	tv_shows.title,
+	tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
