2e0aa6eea4be44f6893c80201bff5994e5f9250c
root <manuelvallejos2018@gmail.com>
2023-12-10 12:01:53 -0800

    Enumera los programas contenidos en 'hbtn_0d_tvshows'

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,8 @@
+-- Se importa el volcado de la database de hbtn_0d_tvshows a mi servidor MYSQL.
 -- Enumera todos los programas contenidos en hbtn_0d_tvshows
---Que tengan al menos un genero vinculado.
+-- Que tengan al menos un genero vinculado.
 
-SELECT 
-	tv_shows.title,
-	tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
