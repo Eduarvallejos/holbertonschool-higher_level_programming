6f283ddd9a140532497448092ce1408b5ca32de1
root <manuelvallejos2018@gmail.com>
2023-12-09 19:30:35 -0800

    Crea y da todos los privilegios a user_0d_1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Este script creara un nuevo usuario llamdado user_0d_1 en el servidor MYSQL.
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
