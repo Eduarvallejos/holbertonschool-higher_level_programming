10434a419cb1b0bac361841f96b4d0b28a6750d8
root <manuelvallejos2018@gmail.com>
2023-12-10 12:27:31 -0800

    Enumera todas las ciudades de California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,9 @@
 -- Enumera todad las ciudades de california que se puededan encontrar dentro de la database 'hbtn_0d_usa'.
 
-USE hbtn_0d_usa;
 
 SELECT cities.id, cities.name
 FROM cities
-WHERE cities.state_id = (
+WHERE state_id = (
     SELECT id
     FROM states
     WHERE name = 'California'
