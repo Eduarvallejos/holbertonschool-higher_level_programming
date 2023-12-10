0ac48cd0677c4b7dd9135d217bee852300144bb7
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-12-10 10:24:52 -0500

    Crea una tabla con valor de columna(id) predeterminado

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- Este script creara una tabla de nombre id_not_null.
--- la fila id tendra un valor predeterminado.
+-- la columna id tendra un valor predeterminado.
 
 CREATE TABLE IF NOT EXISTS id_not_null (
 	id INT DEFAULT '1' NOT NULL,
