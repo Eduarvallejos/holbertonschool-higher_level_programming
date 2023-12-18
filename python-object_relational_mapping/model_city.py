ebf96e828727bb4babd8fa5017196cdc017642ab
Eduarvallejos <135778281+Eduarvallejos@users.noreply.github.com>
2023-12-18 00:32:08 -0500

    ciudades en el estado

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,9 +3,14 @@
 Este módulo define la clase Ciudad.
 """
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+
+Base = declarative_base()
+
 
 class City(Base):
     """
@@ -13,9 +18,6 @@ class City(Base):
     """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    
-    state = relationship("State", back_populates="cities")
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False
