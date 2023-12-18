713bd54c6b4484b947ef727f2c6a8005877641bd
root <manuelvallejos2018@gmail.com>
2023-12-17 18:01:03 -0800

    A salvo de las MySQL injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Este script hace que nos conectemos con Mysql.
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def search_state_by_name():
+    """
+    Esta función se conecta al servidor MySQL recupera y muestra
+    todos los valores en la tabla de estados donde el nombre coincide
+    con el argumento proporcionado y tambien evita las Mysql injections.
+    """
+
+    mysql_user = argv[1]
+    mysql_pass = argv[2]
+    mysql_db = argv[3]
+    host_mysql = 'localhost'
+    port_mysql = 3306
+    state_name = argv[4]
+
+    database = MySQLdb.connect(
+        host=host_mysql,
+        port=port_mysql,
+        user=mysql_user,
+        password=mysql_pass,
+        database=mysql_db
+    )
+
+    cursor = database.cursor()
+    query = "SELECT * FROM states WHERE BINARY\
+            name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    database.close()
+
+
+if __name__ == "__main__":
+    """
+    Esta validación evita que se ejecute
+    este archivo.
+    """
+    search_state_by_name()
