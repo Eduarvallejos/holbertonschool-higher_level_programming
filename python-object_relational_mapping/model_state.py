7fd28f4716991628478cef38cf365bd076239eaf
root <manuelvallejos2018@gmail.com>
2023-12-17 19:32:51 -0800

    Primer modelo de Estado

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Este script define la clase State y crea la instancia Base.
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Clase de estado vinculada a la tabla MySQL 'states'."""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
