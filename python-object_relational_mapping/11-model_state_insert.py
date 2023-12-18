ac553cfdf019bcbe2b3dc34fa8d4d31b76eb36cc
root <manuelvallejos2018@gmail.com>
2023-12-17 20:52:49 -0800

    Agregar un nuevo estado

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Este módulo se conecta a MySql a través sqlalchmemy(ORM).
+Realiza una INSERT en states en hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    """
+    Esta validación es para evitar que el archivo
+    se ejecute al import, Se realiza uan INSERT
+    en states en hbtn_0e_6_usa.
+    """
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = 3306
+
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{password}@{host}:{port}/{db_name}',
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+
+    print(f'{new_state.id}')
