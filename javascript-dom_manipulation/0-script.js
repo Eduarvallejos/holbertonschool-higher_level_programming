ff95685d74e850865d602979c158e9fc13782c80
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-03-15 00:01:16 -0500

    changing the text color

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,5 +1,5 @@
 // Select header element using document.querySelector
-const headerElement = document.querySelector('header')
+const headerElement = document.querySelector('header');
 
 // Update header text color to red (#FF0000)
-headerElement.style.color = '#FF0000'
+headerElement.style.color = '#FF0000';
