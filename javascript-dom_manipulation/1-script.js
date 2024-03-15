1a9a3925b6622e09d555c5e48f00773c8f151a67
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-03-15 00:01:59 -0500

    changes color when you click

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,5 +1,8 @@
+// Select the header element with id 'red_header'
 const header = document.getElementById('red_header');
 
+// Add an event listener for the 'click' event to the header element
 header.addEventListener('click', function () {
-    header.style.color = '#FF0000';
-})
+  // When the header element is clicked, it changes the text color to red (#FF0000)
+  header.style.color = '#FF0000';
+});
