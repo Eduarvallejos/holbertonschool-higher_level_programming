2e2eb89fbc0a80c6fd185f1c8f3d89198d346b6c
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-03-31 15:18:17 -0500

    No JQuery

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+// Wait for the DOM content to be fully loaded before executing the code
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the <header> element in the document and assign it to the variable headerElement
+  const headerElement = document.querySelector('header');
+  // Set the color of the header element to red (#FF0000)
+  headerElement.style.color = '#FF0000';
+});
