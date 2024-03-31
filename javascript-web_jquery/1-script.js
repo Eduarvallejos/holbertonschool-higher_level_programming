6817d09cad35f53337c3b53589198560ac73f4d4
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-03-31 15:56:26 -0500

    With JQuery

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Wait for the DOM content to be fully loaded before executing the code
+$(document).ready(function () {
+  // Select the <header> element using the jQuery API and update its text color to red
+  $('header').css('color', '#FF0000');
+});
